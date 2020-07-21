78ebf8fa1518683d9d38a2151b9763c70c23ddc1
Hayuna <marek.kowalonek@gmail.com>
2020-07-21 16:33:01 +0200

    [refactor] SnippetList.js

--- browser/main/modals/PreferencesModal/SnippetList.js
+++ browser/main/modals/PreferencesModal/SnippetList.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import styles from './SnippetTab.styl'
 import CSSModules from 'browser/lib/CSSModules'
 import dataApi from 'browser/main/lib/dataApi'
@@ -6,69 +6,63 @@ import i18n from 'browser/lib/i18n'
 import eventEmitter from 'browser/main/lib/eventEmitter'
 import context from 'browser/lib/context'
 
-class SnippetList extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      snippets: []
-    }
-  }
+const SnippetList = ({ onSnippetSelect, currentSnippet, onSnippetDeleted }) => {
+  const [snippets, setSnippets] = useState([])
+  const snippetListRef = useRef(null)
 
-  componentDidMount() {
-    this.reloadSnippetList()
-    eventEmitter.on('snippetList:reload', this.reloadSnippetList.bind(this))
-  }
+  useEffect(() => {
+    reloadSnippetList()
+    eventEmitter.on('snippetList:reload', reloadSnippetList)
+  }, [])
 
-  reloadSnippetList() {
+  const reloadSnippetList = () => {
     dataApi.fetchSnippet().then(snippets => {
-      this.setState({ snippets })
-      this.props.onSnippetSelect(this.props.currentSnippet)
+      setSnippets(snippets)
+      onSnippetSelect(currentSnippet)
     })
   }
 
-  handleSnippetContextMenu(snippet) {
+  const handleSnippetContextMenu = (snippet) => {
     context.popup([
       {
         label: i18n.__('Delete snippet'),
-        click: () => this.deleteSnippet(snippet)
+        click: () => deleteSnippet(snippet)
       }
     ])
   }
 
-  deleteSnippet(snippet) {
+  const deleteSnippet = (snippet) => {
     dataApi
       .deleteSnippet(snippet)
       .then(() => {
-        this.reloadSnippetList()
-        this.props.onSnippetDeleted(snippet)
+        reloadSnippetList()
+        onSnippetDeleted(snippet)
       })
       .catch(err => {
         throw err
       })
   }
 
-  handleSnippetClick(snippet) {
-    this.props.onSnippetSelect(snippet)
+  const handleSnippetClick = (snippet) => {
+    onSnippetSelect(snippet)
   }
 
-  createSnippet() {
+  const createSnippet = () => {
     dataApi
       .createSnippet()
       .then(() => {
-        this.reloadSnippetList()
+        reloadSnippetList()
         // scroll to end of list when added new snippet
         const snippetList = document.getElementById('snippets')
-        snippetList.scrollTop = snippetList.scrollHeight
+        snippetListRef.scrollTop = snippetList.scrollHeight
       })
       .catch(err => {
         throw err
       })
   }
 
-  defineSnippetStyleName(snippet) {
-    const { currentSnippet } = this.props
-
-    if (currentSnippet == null) {
+  const defineSnippetStyleName = (snippet) => {
+    if (!currentSnippet) {
       return 'snippet-item'
     }
 
@@ -79,35 +73,32 @@ class SnippetList extends React.Component {
     }
   }
 
-  render() {
-    const { snippets } = this.state
-    return (
-      <div styleName='snippet-list'>
-        <div styleName='group-section'>
-          <div styleName='group-section-control'>
-            <button
-              styleName='group-control-button'
-              onClick={() => this.createSnippet()}
-            >
-              <i className='fa fa-plus' /> {i18n.__('New Snippet')}
-            </button>
-          </div>
+  return (
+    <div styleName='snippet-list'>
+      <div styleName='group-section'>
+        <div styleName='group-section-control'>
+          <button
+            styleName='group-control-button'
+            onClick={createSnippet}
+          >
+            <i className='fa fa-plus' /> {i18n.__('New Snippet')}
+          </button>
         </div>
-        <ul id='snippets' styleName='snippets'>
-          {snippets.map(snippet => (
-            <li
-              styleName={this.defineSnippetStyleName(snippet)}
-              key={snippet.id}
-              onContextMenu={() => this.handleSnippetContextMenu(snippet)}
-              onClick={() => this.handleSnippetClick(snippet)}
-            >
-              {snippet.name}
-            </li>
-          ))}
-        </ul>
       </div>
-    )
-  }
+      <ul ref={snippetListRef} styleName='snippets'>
+        {snippets.map(snippet => (
+          <li
+            styleName={defineSnippetStyleName(snippet)}
+            key={snippet.id}
+            onContextMenu={() => handleSnippetContextMenu(snippet)}
+            onClick={() => handleSnippetClick(snippet)}
+          >
+            {snippet.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
 
 export default CSSModules(SnippetList, styles)
